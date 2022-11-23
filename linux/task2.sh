8506d7330ada386da62a1d19ed41a5e22a0053ac
gopalrathod8 <47740154+gopalrathod8@users.noreply.github.com>
2022-11-23 19:40:26 +0530

    task 2 & 3.

---
+++ linux/task2.sh
@@ -0,0 +1,6 @@
+ grep -r "10" /home/admin/Task_scripts/
+ grep -R 'read' /home/vivek/
+ grep -R 'put' /home/admin/Task_scripts/*
+ grep -H -r "input" /home/admin/Task_scripts/
+ grep -o '\b\w\{6\}\b' /home/admin/Task_scripts/Task_list.txt
+ 
