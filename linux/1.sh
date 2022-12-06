6c72ca5a7c676c667bae4744e4f556926ce7fb1d
kalyan <ayagnesh1@gmail.com>
2022-12-06 22:58:24 +0530

    editing script files

---
+++ linux/1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+class=AWS_Class
+
+echo "Enter your name"
+read name 
+echo "Enter your roll number"
+read rollnumber
+
+
+echo "Hello dear $name your roll number is $rollbumber, welcome to $class"
---
+++ linux/variable.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "this is variable one $1"
+
+echo "this is variable two $2 "
+
+echo "this is variable three $3 "
+
+echo "this is variable four $4 "
+
+echo "this is variable five $5 "
+
+echo "this is variable six $6 "
+
+echo "this is variable seven $7 "
+
+echo "this is variable eight $1 "
+
+echo "this is variable nine $9 "
+
+echo "this is tenth variable ${10}"
+
+echo "total perameter $#"
+
+echo "variable perameters used $@"
