c4d20d2fe4382e3ca02b23d5def8d398e9e6fe71
kalyan <ayagnesh1@gmail.com>
2022-12-07 12:08:46 +0530

    file editing

---
+++ linux/4.sh
@@ -0,0 +1,14 @@
+#!/bin/bassh
+
+echo "enter a phonenumber"
+read phonenumber
+
+if [ $phonenumber == 9989013297 ]
+
+then
+   echo "your phonenumber is correct"
+
+else 
+  echo "your phonenumber is wrong" 
+
+fi`
