6bb99125804178dc43f97c2ff73b7bb5aa2ca28b
kalyan <ayagnesh1@gmail.com>
2022-12-07 11:18:34 +0530

    edit kokila.sh

---
+++ linux/if.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter your user name"
+
+read username
+
+if [ $username == 'yagnesh' ]
+
+
+then 
+   echo "welcome yagnesh"
+
+else 
+   echo "entered incorrect usernam"
+
+fi
---
+++ linux/kokila.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "enter your password"
+read password
+
+if [ $password == '123' ] 
+
+then
+   echo "your password is correct"
+else 
+   echo "your password is wrong"
+
+fi
