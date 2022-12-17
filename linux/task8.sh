ee30c6bd463ff32730c5f75d63d490c052a00ac4
Ashutosh9909 <apati9909@gmail.com>
2022-12-17 20:03:17 +0530

    Task_8 added

---
+++ linux/task8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please enter username"
+read uname
+echo "Please enter Password"
+read  upwd
+if [[ $upwd =~ [^a-zA-Z]+$ ]]
+
+#if [[ $pwd =~ [?=.*[^a-zA-Z0-9_]]+$ ]]
+
+then 
+	echo "Password Accepted. Your username is, $uname and Password you know it"
+else
+	echo "Please provide password with Upper case lowercase and a Number in it"
+fi
